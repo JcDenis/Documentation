f36c1530e1008b27f579dcb4703c73e4a96acc2c
“philippe <philippe.mail.perso@gmail.com>
2025-12-19 05:03:39 +0100

    release 0.8.7

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     'Documentation',
     'Use special templates for documentation posts and categories',
     'Jean-Christian Paul Denis and Contributors',
-    '0.8.6',
+    '0.8.7',
     [
         'requires'    => [
             ['core', '2.36'],
