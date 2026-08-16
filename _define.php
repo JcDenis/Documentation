3f97745eda552a8b5dbb8d586bfc954dfa99c133
Jean-Christian Denis <contact@jcdenis.fr>
2026-08-16 15:07:47 +0200

    Release 0.10.1

--- _define.php
+++ _define.php
@@ -22,7 +22,7 @@ $this->registerModule(
     'Documentation',
     'Use special templates for documentation posts and categories',
     'Jean-Christian Paul Denis and Contributors',
-    '0.10',
+    '0.10.1',
     [
         'requires'    => [
             ['core', '2.39'],
